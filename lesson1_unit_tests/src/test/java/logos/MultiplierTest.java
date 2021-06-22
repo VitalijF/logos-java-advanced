90a95b725491085b760d3c8465adc5245fcb7ac7
SvitlanaBilous87 <s_vitlanka@ukr.net>
2021-06-22 12:18:16 +0300

    Homework Unit tests Svitlana Bilous

--- lesson1_unit_tests/src/test/java/logos/MultiplierTest.java
+++ lesson1_unit_tests/src/test/java/logos/MultiplierTest.java
@@ -17,10 +17,10 @@ public class MultiplierTest {
     //TODO: пофіксити
     @ParameterizedTest
     @CsvSource(value = {
-            "1, 1, 2",
+            "1, 1, 1",
             "2, 1, 2",
             "3, 5, 15",
-            "4, 2, 5",
+            "4, 2, 8",
             "1, 10, 10",
             "5, 5, 25",
     })
@@ -32,12 +32,12 @@ public class MultiplierTest {
     //TODO: пофіксити
     @ParameterizedTest
     @CsvSource(value = {
-            "1, 1, 1, 2",
+            "1, 1, 1, 1",
             "2, 1, 5, 10",
             "3, 5, 1, 15",
-            "4, 2, 7, 50",
-            "1, 10, 2, 10",
-            "5, 5, 2, 45",
+            "4, 2, 7, 56",
+            "1, 10, 2, 20",
+            "5, 5, 2, 50",
     })
     public void testMultipleThreeNumbers(double firstNumber, double secondNumber, double thirdNumber, double expected) {
         double actual = multiplier.multiply(firstNumber, secondNumber, thirdNumber);
@@ -46,13 +46,29 @@ public class MultiplierTest {
 
     // TODO: Написати тест кейси для positiveMultiply (3 кейси) для випадку коли якесь з чисел або обидва є відємні
     @ParameterizedTest
-    public void testPositiveMultiplyError() {
-
+    @CsvSource(value = {
+            "1, -1",
+            "-2, 1",
+            "-3, -5",
+    })
+    public void testPositiveMultiplyError(double firstNumber, double secondNumber) {
+        IllegalArgumentException exception = Assertions.assertThrows(IllegalArgumentException.class,
+                () -> multiplier.positiveMultiply(firstNumber, secondNumber));
+        Assertions.assertEquals("Only positive number can be multiplied", exception.getMessage());
     }
 
     // TODO: Написати тест кейси для positiveMultiply (мінімум 5) для випадку коли два числа додатні
     @ParameterizedTest
-    public void testPositiveMultiply() {
-
+    @CsvSource(value = {
+            "6, 6, 36",
+            "100, 5, 500",
+            "30, 5, 150",
+            "41, 2, 82",
+            "2, 11, 22",
+            "3, 7, 21",
+    })
+    public void testPositiveMultiply(double firstNumber, double secondNumber, double expected) {
+        double actual = multiplier.positiveMultiply(firstNumber, secondNumber);
+        Assertions.assertEquals(expected, actual);
     }
 }
--- lesson1_unit_tests/src/test/java/logos/StringHelperTest.java
+++ lesson1_unit_tests/src/test/java/logos/StringHelperTest.java
@@ -1,27 +1,126 @@
 package logos;
 
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.*;
+
+import java.util.stream.Stream;
+
 public class StringHelperTest {
 
+    private StringHelper stringHelper;
+
+    @BeforeEach
+    public void init() {
+        stringHelper = new StringHelper();
+    }
 
     //Teсткейс або тесткейси для методу isEmpty() який повинен повернути true
-    void testStringIsNotEmpty() {
-
+    @ParameterizedTest
+    @NullAndEmptySource
+    void testStringIsEmpty(String string) {
+        Assertions.assertTrue(stringHelper.isEmpty(string));
     }
 
     //Teсткейс або тесткейси для методу isEmpty() який повинен повернути false
-    void testStringIsEmpty() {
-
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "test",
+            ",",
+            "123",
+            " "
+    })
+    void testStringIsNotEmpty(String string) {
+        Assertions.assertFalse(stringHelper.isEmpty(string));
     }
 
     //Teсткейс або тесткейси для методу getWithoutFirstSecondSymbols() який повинен повернути стрічку без перших двох символів
-    void testGetWithoutFirstSecondSymbols() {
-
+    @ParameterizedTest
+    @CsvSource(value = {
+            "Test, st",
+            "bicycle, cycle",
+            "defrost, frost",
+    })
+    void testGetWithoutFirstSecondSymbols(String string, String expected) {
+        String actual = stringHelper.getWithoutFirstSecondSymbols(string);
+        Assertions.assertEquals(expected, actual);
     }
 
     //Teсткейс або тесткейси для методу getWithoutFirstSecondSymbols() який повинен викинути ексепшин
-    void testGetWithoutFirstSecondSymbolsException() {
-
+    @ParameterizedTest
+    @NullAndEmptySource
+    @ValueSource(strings = {
+            "t",
+            ","
+    })
+    void testGetWithoutFirstSecondSymbolsException(String string) {
+        IllegalArgumentException exception = Assertions.assertThrows(IllegalArgumentException.class,
+                () -> stringHelper.getWithoutFirstSecondSymbols(string));
+        Assertions.assertEquals("String should contain 2 or more symbols", exception.getMessage());
     }
 
     //Tecткейси для методу concatenateTwoStringsWithDelimiter
+    @ParameterizedTest
+    @CsvSource(value = {
+            "First, test, First_test",
+            "2, test, 2_test",
+            "1, 5, 1_5"
+    })
+    void testConcatenateTwoStringsWithDelimiter(String firstString, String secondString, String expected) {
+        String actual = stringHelper.concatenateTwoStringsWithDelimiter(firstString, secondString);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    //Tесткейси для методу concatenateTwoStringsWithDelimiter які повинні викинути ексепшин на пусту стрічку
+    //Варіант 1
+    @ParameterizedTest
+    @CsvSource(value = {
+            "test1, ''",
+            "'', test2",
+            "'',''",
+            ", test3",
+            "test4, ",
+            ","
+    })
+    void testConcatenateTwoStringsWithDelimiterEmptyException1(String firstString, String secondString) {
+        IllegalArgumentException exception = Assertions.assertThrows(IllegalArgumentException.class,
+                () -> stringHelper.concatenateTwoStringsWithDelimiter(firstString, secondString));
+        Assertions.assertEquals("Strings must not be empty", exception.getMessage());
+    }
+
+    //Tесткейси для методу concatenateTwoStringsWithDelimiter які повинні викинути ексепшин на пусту стрічку
+    //Варіант 2
+    @ParameterizedTest
+    @MethodSource("stringsForTestProvider")
+    void testConcatenateTwoStringsWithDelimiterEmptyException2(String firstString, String secondString) {
+        IllegalArgumentException exception = Assertions.assertThrows(IllegalArgumentException.class,
+                () -> stringHelper.concatenateTwoStringsWithDelimiter(firstString, secondString));
+        Assertions.assertEquals("Strings must not be empty", exception.getMessage());
+    }
+
+    static Stream<Arguments> stringsForTestProvider() {
+        return Stream.of(
+                Arguments.arguments("test1", null),
+                Arguments.arguments(null, "test2"),
+                Arguments.arguments(null, null),
+                Arguments.arguments("test3", ""),
+                Arguments.arguments("", "test4"),
+                Arguments.arguments("", "")
+        );
+    }
+
+    //Tесткейси для методу concatenateTwoStringsWithDelimiter які повинні викинути ексепшин на однакові стрічки
+    @ParameterizedTest
+    @CsvSource(value = {
+            "test, test",
+            "1, 1"
+    })
+    void testConcatenateTwoStringsWithDelimiterEqualStringsException(String firstString, String secondString) {
+        IllegalArgumentException exception = Assertions.assertThrows(IllegalArgumentException.class,
+                () -> stringHelper.concatenateTwoStringsWithDelimiter(firstString, secondString));
+        Assertions.assertEquals("Strings must be different", exception.getMessage());
+    }
+
 }
